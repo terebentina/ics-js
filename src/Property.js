8768211c1ea22af1b6cfd2d76d19b92f03e1e7d8
Dan Caragea <dancaragea@gmail.com>
2015-08-10 17:06:24 +0300

    split long lines at 75 chars

--- src/Property.js
+++ src/Property.js
@@ -31,6 +31,6 @@ export default class Property {
     const hasProps = Object.keys(this.props).length > 0;
     const key = this.constructor.propName + (hasProps ? ';' + this.transformedProps() : '');
     const value = this.transformedValue();
-    return `${key}:${value}`;
+    return `${key}:${value}`.match(/.{1,75}/g).join('\r\n ');
   }
 }
